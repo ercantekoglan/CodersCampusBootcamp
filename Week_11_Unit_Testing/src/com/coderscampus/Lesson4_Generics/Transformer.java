270e050ed86da5f136e10cc59b518ba2a5dc60dc
Ercan Tekoglan <65437249+ercantekoglan@users.noreply.github.com>
2022-11-21 17:45:28 -0600

    Update Transformer.java

--- Week_11_Unit_Testing/src/com/coderscampus/Lesson4_Generics/Transformer.java
+++ Week_11_Unit_Testing/src/com/coderscampus/Lesson4_Generics/Transformer.java
@@ -4,15 +4,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Transformer {
-	public static <T1, T2> String join(T1 v1, T2 v2) {
+//public class Transformer<T1, T2> {
 
-		return v1.toString() + " --> " + v2.toString();
+// static method bu sekilde hata verir cunku genericler static degil.
+//	public static String join(T1 v1, T2 v2) {
+//		
+//		return v1.toString() + " -> " + v2.toString();
+//	}
+
+	//return  type oncesinde generic yaziyosun.boylece method da generic oluyo
+	public static <T1, T2> String join(T1 v1, T2 v2) {
+	
+		return v1.toString() + " -> " + v2.toString();
 	}
-	//? anything demek. belirli durumlarda nereden extend oldugunu belirtmek gerekli
-	//NUmber a tiklayip f4 yapinca icerik gozukur
+	
+	//wildcard example -- burada listenin Number(number ustunde f4 e basinca hiyersiyi gorursun) turunde olacagini belirtiyoruz
+	//List<?> -> list of anything
 	public static String join(List<? extends Number> list) {
 		
 		return list.stream().map(Number::toString).collect(Collectors.joining("\n"));
 	}
-
 }
+
